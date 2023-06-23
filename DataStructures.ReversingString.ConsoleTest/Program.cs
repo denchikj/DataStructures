a648d31e5638baab9423e2d19f8cd8bd4488b439
denchikj <igoshevdenis9876@gmail.com>
2023-06-23 21:21:34 +0500

    ReversingString

---
+++ DataStructures.ReversingString.ConsoleTest/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DataStructures.ReversingString.ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "jsdag dgpdg ioetg ipgh k";
+            Console.WriteLine(Implementations.ReversingString.ReverseString(str));
+        }
+    }
+}
---
+++ DataStructures.ReversingString/ReversingString.cs
@@ -0,0 +1,14 @@
+﻿namespace DataStructures.ReversingString.Implementations
+{
+    public class ReversingString
+    {
+        public static string ReverseString(string str)
+        {
+            string result = string.Empty;
+            for (int i = str.Length - 1; i >= 0; i--)
+                result += str[i];
+
+            return result;
+        }
+    }
+}
