6cf0a0dceaa7eff325861a174e046cb955f5bc18
denchikj <igoshevdenis9876@gmail.com>
2023-06-24 21:52:09 +0500

    MergeSortedArrays

---
+++ DataStructures.MergeSortedArray.ConsoleTest/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DataStructures.MergeSortedArray.ConsoleTest
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 3, 5, 7, 9};
+            int n1 = arr1.Length;
+
+            int[] arr2 = { 2, 4, 6, 8, 10 };
+            int n2 = arr2.Length;
+
+            int[] arr3 = new int[n1 + n2];
+
+            Implementations.MergeSortedArrays.MergeArrays(arr1, arr2, n1, n2, arr3);
+
+            Console.Write("Array after merging\n");
+            for (int i = 0; i < n1 + n2; i++)
+                Console.Write(arr3[i] + " ");
+        }
+    }
+}
