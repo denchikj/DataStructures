e490cf5d2361c117d5cc589e9163912b91e76864
denchikj <igoshevdenis9876@gmail.com>
2023-06-24 22:24:51 +0500

    HashTable

--- DataStructures.HashTable.ConsoleTest/Program.cs
+++ DataStructures.HashTable.ConsoleTest/Program.cs
@@ -6,16 +6,14 @@ namespace DataStructures.HashTable.ConsoleTest
     {
         static void Main(string[] args)
         {
-            Implementations.HashTable ht = new Implementations.HashTable(3);
+            Implementations.HashTable ht = new Implementations.HashTable(2);
             ht.Set("banana", 200);
             ht.Set("pear", 100);
-            ht.Set("pear", 1200);
 
             foreach (var item in ht.Keys())
             {                
                 Console.WriteLine(ht.Get(item));
             }
-            //Console.WriteLine(ht.Get("pear"));
         }
     }
 }
